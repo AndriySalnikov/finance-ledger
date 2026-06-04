9b27134f94533ef34dc62cf86ab72bc36ea2d6df
Andriy Salnikov <a.salnikov.home@outlook.com>
2026-06-04 13:36:14 +0200

    Update configs

--- src/main/java/ua/pp/salnikov/financeledger/repository/AccountRepository.java
+++ src/main/java/ua/pp/salnikov/financeledger/repository/AccountRepository.java
@@ -1,8 +1,10 @@
 package ua.pp.salnikov.financeledger.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ua.pp.salnikov.financeledger.entity.Account;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
 }
